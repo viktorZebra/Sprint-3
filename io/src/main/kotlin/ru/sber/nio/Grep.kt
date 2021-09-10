c4d731a936bac7294e43e9a1bfab0908815302c3
Artem <viktorzebra54@gmail.com>
2021-09-10 13:13:55 +0300

    done nio

--- io/src/main/kotlin/ru/sber/nio/Grep.kt
+++ io/src/main/kotlin/ru/sber/nio/Grep.kt
@@ -1,5 +1,12 @@
 package ru.sber.nio
 
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.io.path.isRegularFile
+import kotlin.io.path.useLines
+
+
 /**
  * Реализовать простой аналог утилиты grep с использованием калссов из пакета java.nio.
  */
@@ -14,7 +21,36 @@ class Grep {
      * Пример для подстроки "22/Jan/2001:14:27:46":
      * 22-01-2001-1.log : 3 : 192.168.1.1 - - [22/Jan/2001:14:27:46 +0000] "POST /files HTTP/1.1" 200 - "-"
      */
-    fun find(subString: String) {
+    fun find(subString: String, inputFilePath: String = "C:\\Student_work\\Sprint-3\\io\\logs", outputFilePath: String = "C:\\Student_work\\Sprint-3\\io\\result.txt") {
+        val path = Paths.get(inputFilePath)
+        val outputFile = File(outputFilePath)
+
+        try {
+            outputFile.bufferedWriter().use {
+                outputStream ->
+                Files.walk(path).filter { it.isRegularFile() }.forEach {
+                    file ->
+                    file.useLines {
+                        line ->
+                        line.filter {
+                            currLine ->
+                            currLine.contains(subString)
+                        }.forEach {
+                            string ->
+                            outputStream.write("${file.fileName} : ${file.useLines { it.indexOf(string) } + 1} : $string")
+                        }
+                    }
+                }
+            }
+        } catch (e: Exception)
+        {
+            println(e.message)
+        }
 
     }
+}
+
+fun main(){
+    val grep: Grep = Grep()
+    grep.find("22/Jan/2001:14:27:46")
 }
